5779c56f0066b032ab6dd020597b05fdce04e6b7
sara ahmed <sara.bakr.ahmad@gmail.com>
2022-10-29 21:29:06 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Sara';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + " !";
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const userInitial = currentUser.slice(0, 1);
+
+const shortGreeting = `Welcome, ${userInitial}!`;
